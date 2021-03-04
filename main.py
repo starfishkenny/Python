76b04196929b7597bf92d874e4b7af5568b76d01
starfishkenny <starfish8913@naver.com>
2021-03-04 23:10:14 +0900

    completed chapter 2

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
+'''
 import module01
 ret = module01.sum(10,20)
+'''
 
+'''
 import module02
 print('Main run')
+'''
 
+'''
 import module03
 print('Main run')
 module03.viewPI()
 module03.PI = 3.141592 # PI 변수의 값을 변경할 수도 있음
 module03.viewPI()
+'''
 
+'''
 import module03
 print('Main run')
 module03.angle = 10.4
 print(module03.angle) # module03.py에는 angle 함수가 없으나 파이썬은 동적임 (미리 작성된 py 파일을 import 하더라도 실행 시 해당 모듈 내에 변수를 추가로 생성할 수 있음)
+'''
 
+'''
 import module03
 print('Main run')
 module03.angle = 10.24 # module03.py 내에 angle 변수를 정의하는 것이 더욱 '자연스러운' 코드가 될 것임
 ret = module03.transAngle()
 print(ret)
+'''
 
+'''
 import module04
 module04.setCode(10)
 ret = module04.getCode() # code 값이 안변함 (파이썬은 함수 안에서 변수를 사용하면 기본적으로 "지역" 개념에서 다룸)
 print(ret) # 함수 밖에 선언된 같은 이름의 변수가 있더라도 대입연산 시 함수 내부에 새로운 변수를 만드는 개념
+'''
 
 # 폴더가 다른 모듈 파일 import 하기
 
@@ -63,8 +75,10 @@ ret = total(a)
 print(ret)
 '''
 
+'''
 import sys # 파이썬 라이브러리가 설치되어 있는 폴더들 확인
 print(sys.path)
+'''
 
 # 모듈이 위의 경로에 있는 경우에는 폴더를 이동하거나 폴더명을 작성할 필요 없이 모듈명만을 적어 사용할 수 있음
 # 자신이 사용할 폴더는 추가하기 위해 sys.path.append('추가경로')를 실행하면 바로 모듈명으로 import하여 사용할 수 있음
@@ -126,5 +140,81 @@ gotcha.sleep()
 jimbar.sleep()
 swan.sleep()
 '''
+
+'''
 from classes import *
 gotcha = Pokemon()
+'''
+
+'''
+from classes import *
+poke1 = Pokemon('갓챠')
+poke2 = Pokemon('읏챠')
+print(poke1.scores)
+print(poke2.scores)
+poke1.scores.append(2)
+print(poke1.scores)
+print(poke2.scores) # poke1에만 추가되어야 하는데 poke2에도 값이 추가됨
+'''
+
+'''
+from classes import *
+poke1 = Pokemon('갓챠')
+poke2 = Pokemon('읏챠')
+print(poke1.scores)
+print(poke2.scores)
+poke1.scores.append(2)
+print(poke1.scores)
+print(poke2.scores) # poke2에는 변화가 없음
+'''
+'''
+from classes import *
+
+gotcha = BattlePokemon('갓챠')
+gotcha.sleep()
+'''
+
+'''
+from classes import *
+
+gotcha = BattlePokemon('갓챠')
+jimba = BattlePokemon('짐바')
+gotcha.sleep()
+jimba.sleep()
+
+gotcha.attack()
+jimba.defense()
+
+jimba.attack()
+gotcha.defense()
+'''
+'''
+from classes import *
+
+kim = Employee()
+kim.view()
+'''
+
+# "직원" 인스턴스 생성 시 '이름'과 '잔화번호'를 전달해야 함
+
+'''
+from classes import *
+
+kim = Employee("홍길동","010-1111-2222")
+minh = Employee("민혁기","010-2222-3333")
+kim.view()
+minh.view()
+'''
+
+from classes import *
+
+company = Company('(주)기운찬','Seoul,Korea','02-1234-5678')
+company.appendDepart('영업팀','02-1234-5677')
+company.appendDepart('개발팀','02-1234-5676')
+company.appendDepart('회계팀','02-1234-5675')
+company.departments[0].appendEmployee('홍길동','010-1111-2222')
+company.departments[0].appendEmployee('민혁기','010-1122-3333')
+company.departments[1].appendEmployee('스무스','010-1133-4444')
+company.departments[2].appendEmployee('강한자','010-1144-5555')
+
+company.view()
